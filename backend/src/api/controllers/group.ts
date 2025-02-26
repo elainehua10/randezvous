5ccc2f9551e8511d720c48383efd2a4fe46e55b1
Bryan Yoo <bryanyoo5433@gmail.com>
2025-02-25 21:49:26 -0500

    created accept invite

--- backend/src/api/controllers/group.ts
+++ backend/src/api/controllers/group.ts
@@ -1,6 +1,6 @@
 // group.ts
 // All the group processing logic
-
+import { Request, Response } from 'express';
 import sql from "../../db";
 
 // Set limit to how many groups a user can create
@@ -9,11 +9,14 @@ const MAX_GROUPS_PER_USER = 5;
 // ============= Leader of group functions ===================
 
 // Create a new group
-const createGroup = async (
-  userId: string,
-  groupName: string,
-  isPublic: boolean
-) => {
+export const createGroup = async (req: Request, res: Response) => {
+  const { userId, groupName, isPublic } = req.body;
+
+  // Validate required fields
+  if (!userId || !groupName || isPublic === undefined) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
   try {
     // Insert new group (user is the leader)
     let res = await sql`
@@ -56,6 +59,52 @@ const inviteToGroup = async (
 // ============= Member of group functions ===================
 
 // Accept invite
+export const acceptInvite = async (req: Request, res: Response) => {
+  const { userId, groupId } = req.body;
+
+  if (!userId || !groupId) {
+    return res.status(400).json({ error: 'Missing required fields' });
+  }
+
+  try {
+    // Check if there's a pending invite for this user and group
+    const invite = await sql`
+      SELECT * FROM invite
+      WHERE group_id = ${groupId} 
+        AND to_user_id = ${userId} 
+        AND status = 'pending'
+      LIMIT 1;
+    `;
+
+    if (!invite) {
+      return res.status(404).json({ error: 'No pending invite found for this user and group'})
+    }
+    
+    // Adding user to the user_group table
+    await sql`
+      INSERT INTO user_group (group_id, user_id)
+      VALUES (${groupId}, ${userId})
+      ON CONFLICT DO NOTHING;
+    `;
+
+    // Update the invite status to 'accepted'
+    await sql`
+      UPDATE invites
+      SET status = 'accepted'
+      WHERE group_id = ${groupId}
+        AND to_user_id = ${userId};
+    `;
+
+    return res.status(200).json({ message: 'Invite accepted. You have been added to the group.' });
+
+  } catch (error) {
+    console.error('Error accepting invite:', error);
+    return res.status(500).json({
+      error: (error as Error).message || 'An unknown error occurred',
+    });
+  }
+};
+
 
 // Leave the a group user is in
 
