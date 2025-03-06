5561b617712693ab3284f17faeb092de6f4183f7
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2025-03-06 13:28:51 -0500

    base group screen
    Signed-off-by: Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/group_screen.dart';
 import 'package:frontend/login.dart';
 import 'package:frontend/register.dart';
-import 'package:frontend/mapscreen.dart';
+import 'package:frontend/map_screen.dart';
 import 'package:frontend/profile.dart';
 
 void main() {
@@ -16,12 +17,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'RandezVous',
       theme: ThemeData(primarySwatch: Colors.yellow),
-      initialRoute: '/login',
+      initialRoute: '/home',
       home: LoginScreen(),
       routes: {
         '/login': (context) => LoginScreen(),
         '/register': (context) => RegisterScreen(),
         '/home': (context) => MapScreen(),
+        '/group': (context) => GroupScreen(groupId: ''),
         '/profile': (context) => ProfileScreen(),
       },
       //home: LoginScreen(),
