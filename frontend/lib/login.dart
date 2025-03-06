cf3406f95274eb639fc269e7ae23df0c62756d5a
Nathanael Aou <nathanaelaou23@gmail.com>
2025-03-06 14:46:54 -0500

    fix: changed mapscreen to map_screen

--- frontend/lib/login.dart
+++ frontend/lib/login.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/mapscreen.dart';
+import 'package:frontend/map_screen.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
--- frontend/lib/register.dart
+++ frontend/lib/register.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/mapscreen.dart';
+import 'package:frontend/map_screen.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
